5c051495c200db8984e78b0857c0a4fcce7bb305
Amirul Islam Papon <99961342+Amirul-Islam-Papon@users.noreply.github.com>
2025-02-17 12:52:24 +0600

    Add files via upload
    FCFS Scheduling

---
+++ fcps_scheduling.py
@@ -0,0 +1,29 @@
+def fcfs_scheduling(processes):
+    processes.sort(key=lambda x: x[1])  
+    
+    n = len(processes)
+    tat = [0] * n
+    wt = [0] * n
+    ct = [0] * n
+
+    ct[0] = processes[0][1] + processes[0][2]  
+    for i in range(1, n):
+        ct[i] = max(ct[i-1], processes[i][1]) + processes[i][2]
+    
+    for i in range(n):
+        tat[i] = ct[i] - processes[i][1]
+        wt[i] = tat[i] - processes[i][2]
+    
+    print("Process\tTAT\tWT")
+    for i in range(n):
+        print(f"{processes[i][0]}\t\t{tat[i]}\t{wt[i]}")
+    
+
+process_list = [
+    ('P1', 0, 4),
+    ('P2', 1, 3),
+    ('P3', 2, 1),
+    ('P4', 3, 2)
+]
+
+fcfs_scheduling(process_list)
